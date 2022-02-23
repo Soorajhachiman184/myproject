31b202022e2f84837c4252a9230bbb6e65fda8e8
VishnuU123 <vishnu.u@xilligence.com>
2022-02-23 17:57:01 +0530

    latest code

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+      <div className = "App" >
+
+        <b> More Actions</b>  
+        <select id = "myList" onchange = "favTutorial()" >  
+        <option> View </option>  
+        <option> Edit </option>  
+        <option> Send </option>  
+        <option style={{color:"red"}}> Delete </option>  
+    
+        </select> 
+<br />
+<br />
+        <b> More Actions</b>  
+        <select id = "myList" onchange = "favTutorial()" >  
+        <option> View </option>  
+        <option> Edit </option>  
+        <option> Send </option>  
+        <option style={{color:"red"}}> Delete </option>  
+    
+        </select> 
+        <b> More Actions</b>  
+        <select id = "myList" onchange = "favTutorial()" >  
+        <option> View </option>  
+        <option> Edit </option>  
+        <option> Send </option>  
+        <option style={{color:"red"}}> Delete </option>  
+    
+        </select> 
+        </div>
+    );
 }
 
 export default App;
