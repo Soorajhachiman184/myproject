e351b15b6404c883b9817c3dd2adc1fb8169b87f
VishnuU123 <vishnu.u@xilligence.com>
2022-02-23 18:16:38 +0530

    code

--- src/App.js
+++ src/App.js
@@ -15,22 +15,9 @@ function App() {
         </select> 
 <br />
 <br />
-        <b> More Actions</b>  
-        <select id = "myList" onchange = "favTutorial()" >  
-        <option> View </option>  
-        <option> Edit </option>  
-        <option> Send </option>  
-        <option style={{color:"red"}}> Delete </option>  
+
     
-        </select> 
-        <b> More Actions</b>  
-        <select id = "myList" onchange = "favTutorial()" >  
-        <option> View </option>  
-        <option> Edit </option>  
-        <option> Send </option>  
-        <option style={{color:"red"}}> Delete </option>  
     
-        </select> 
         </div>
     );
 }
